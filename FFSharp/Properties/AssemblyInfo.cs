0af33235268849a7e7cfaa0105165d2f0ceaf142
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2018-06-22 23:20:03 +0200

    Starting point.

---
+++ FFSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FFSharp")]
+[assembly: AssemblyDescription("Managed .NET wrapper for FFmpeg")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FFSharp")]
+[assembly: AssemblyCopyright("Copyright © Karl F. A. Friebel 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f2e43233-5d7c-4f0a-8a7c-51b79b20f4a0")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
