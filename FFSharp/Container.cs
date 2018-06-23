859db6e27fccd18ed676040dc166f690a0156841
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2018-06-23 16:02:23 +0200

    Added Container.

---
+++ FFSharp/Container.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using FFSharp.Native;
+
+using JetBrains.Annotations;
+
+using Unsafe = FFmpeg.AutoGen;
+
+namespace FFSharp
+{
+    /// <summary>
+    /// Represents a container used in FFmpeg.
+    /// </summary>
+    // ReSharper disable errors
+    [PublicAPI]
+    public sealed class Container
+    {
+        /// <summary>
+        /// Get the internal reference to the <see cref="Unsafe.AVFormatContext"/>.
+        /// </summary>
+        internal Ref<Unsafe.AVFormatContext> Ref;
+
+        /// <summary>
+        /// Create a new <see cref="Container"/> instance.
+        /// </summary>
+        /// <param name="ARef">The <see cref="Unsafe.AVFormatContext"/>.</param>
+        internal Container(Ref<Unsafe.AVFormatContext> ARef)
+        {
+            Ref = ARef;
+        }
+        /// <summary>
+        /// Create a new <see cref="Container"/> instance.
+        /// </summary>
+        public Container()
+            : this(AVFormatContext.Alloc())
+        { }
+    }
+    // ReSharper restore errors
+}
---
+++ FFSharp/Native/AVFormatContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using JetBrains.Annotations;
+
+using Unsafe = FFmpeg.AutoGen;
+
+namespace FFSharp.Native
+{
+    /// <summary>
+    /// Provides methods that operate on <see cref="Unsafe.AVFormatContext"/>.
+    /// </summary>
+    // ReSharper disable errors
+    internal static unsafe class AVFormatContext
+    {
+        /// <summary>
+        /// Allocate a new <see cref="Unsafe.AVFormatContext"/>.
+        /// </summary>
+        /// <returns>A new <see cref="Unsafe.AVFormatContext"/>.</returns>
+        [MustUseReturnValue]
+        public static Ref<Unsafe.AVFormatContext> Alloc()
+        {
+            return FFmpegHelper.CheckAlloc<Unsafe.AVFormatContext>(
+                Unsafe.ffmpeg.avformat_alloc_context()
+            );
+        }
+
+        /// <summary>
+        /// Free a <see cref="Unsafe.AVFormatContext"/>.
+        /// </summary>
+        /// <param name="ARef">The <see cref="Unsafe.AVFormatContext"/>.</param>
+        public static void Free(ref Ref<Unsafe.AVFormatContext> ARef)
+        {
+            // No null-check necessary.
+
+            Unsafe.ffmpeg.avformat_free_context(ARef);
+            ARef = null;
+        }
+    }
+    // ReSharper restore errors
+}
