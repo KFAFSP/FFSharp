7aae68597a1e5878b694189b655c59aa4e91e8d4
Karl Friebel <kfr@inavet.de>
2018-06-27 14:24:18 +0200

    Added setup and teardown for FixedTests.

--- Tests/Native/FixedTests.cs
+++ Tests/Native/FixedTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using System.Runtime.InteropServices;
+
 using NUnit.Framework;
 
 // ReSharper disable ReturnValueOfPureMethodIsNotUsed
@@ -12,8 +14,40 @@ namespace FFSharp.Native
     [Category("Native")]
     [Category("Utilities")]
     [TestOf(typeof(Fixed<>))]
-    public class FixedTests
+    public unsafe class FixedTests
     {
+        public unsafe struct Unmanaged
+        {
+            public struct Nested
+            {
+                bool val;
+            }
 
+            public int a;
+            public Nested b;
+            public Nested* c;
+        }
+
+        Unmanaged* FStruct;
+
+        Fixed<Unmanaged> FNull;
+        Fixed<Unmanaged> FFixed;
+        Fixed<Unmanaged> FDefault;
+
+        [SetUp]
+        public void Setup()
+        {
+            FStruct = (Unmanaged*)Marshal.AllocHGlobal(Marshal.SizeOf<Unmanaged>());
+
+            FNull = Fixed<Unmanaged>.Null;
+            FFixed = new Fixed<Unmanaged>(FStruct);
+            FDefault = default;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Marshal.FreeHGlobal((IntPtr)FStruct);
+        }
     }
 }
