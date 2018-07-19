06e9b37697c5da6f862368bbe396e5d10739dd8b
Karl Friebel <kfr@inavet.de>
2018-07-19 17:20:55 +0200

    Added AVIO.

---
+++ FFSharp/Native/AVIOSeekFlags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Unsafe = FFmpeg.AutoGen;
+
+namespace FFSharp.Native
+{
+    /// <summary>
+    /// Flags for <see cref="Unsafe.ffmpeg.avio_seek(Unsafe.AVIOContext*, long, int)"/> operations.
+    /// </summary>
+    [Flags]
+    internal enum AVIOSeekFlags
+    {
+        /// <summary>
+        /// No flags.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Force seeking if possible (reopen, linear reading, ...).
+        /// </summary>
+        Force = Unsafe.ffmpeg.AVSEEK_FORCE,
+        /// <summary>
+        /// Instead of seeking, return the file size.
+        /// </summary>
+        Size = Unsafe.ffmpeg.AVSEEK_SIZE
+    }
+    // ReSharper restore errors
+}
---
+++ FFSharp/Native/AVIOSeekOrigin.cs
@@ -0,0 +1,24 @@
+﻿using Unsafe = FFmpeg.AutoGen;
+
+namespace FFSharp.Native
+{
+    /// <summary>
+    /// Origin for <see cref="Unsafe.ffmpeg.avio_seek(Unsafe.AVIOContext*, long, int)"/> operations.
+    /// </summary>
+    internal enum AVIOSeekOrigin
+    {
+        /// <summary>
+        /// Seek from the start of the file.
+        /// </summary>
+        Begin = 0,
+        /// <summary>
+        /// Seek from the current position in the file.
+        /// </summary>
+        Current = 1,
+        /// <summary>
+        /// Seek from the end of the file.
+        /// </summary>
+        End = 2
+    }
+    // ReSharper restore errors
+}
